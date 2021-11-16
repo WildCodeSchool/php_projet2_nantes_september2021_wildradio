71332d9886772408143f7f6edf8a4109b9a69606
Johan Mabit <50822877+Johan-Mb@users.noreply.github.com>
2021-11-16 10:32:53 +0100

    fix page Playlist

--- src/Controller/admin/TrackController.php
+++ src/Controller/admin/TrackController.php
@@ -80,7 +80,7 @@ class TrackController extends AbstractController
             $uploadFile = $uploadDir . uniqid() . "." . $extension;
 
             // Le poids max géré par PHP
-            $maxFileSize = 5000000;
+            $maxFileSize = 50000000;
   
             if (file_exists($_FILES['mp3']['tmp_name']) && filesize($_FILES['mp3']['tmp_name']) > $maxFileSize) {
                 $this->errors["mp3"] ="le poid max du fichier est de 5Mo";} 
